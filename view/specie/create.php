d32cae3e12070445861830db13b65f82bc6c6806
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-06-07 11:11:19 +0200

    Update

--- view/specie/create.php
+++ view/specie/create.php
@@ -1,9 +1,9 @@
 <div class="container">
-	<h1>Species Toevoegen</h1>
+	<h1>Specie Toevoegen</h1>
 		<form action="<?= URL ?>specie/createSave" method="post">
 			<div>
-	          <input type="text" name="Animal">
-	          <label for="Animal">Dier</label>
+	          <input type="text" name="description">
+	          <label for="description">Dier</label>
 	        </div>
 			<input type="submit" value="Toevoegen">
 			<a href="<?= URL ?>specie/index">Annuleer</a>
