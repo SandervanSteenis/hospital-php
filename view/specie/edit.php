88a817b7f3935711f202a63d5a481536bc21cbc9
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-06-13 11:31:41 +0200

    Update

--- view/specie/edit.php
+++ view/specie/edit.php
@@ -2,8 +2,8 @@
 	<h1>Specie Wijzigen</h1>
 		<form action="<?= URL ?>specie/editSave" method="post">
 			<div>
-	          <input id="name" type="text" name="Animal" value="<?= $specie['species_description']; ?>">
-	          <label for="Animal">Dier</label>
+	          <input id="name" type="text" name="description" value="<?= $specie['species_description']; ?>">
+	          <label for="description">Diersoort</label>
 	        </div>
 			<input type="hidden" name="id" value="<?= $specie['species_id']; ?>">
             <input type="submit" value="Bijwerken">
